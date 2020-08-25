3663895f20733127583f45d28ea96cae3966aaf2
malyvsen <5940672+malyvsen@users.noreply.github.com>
2020-08-25 11:43:02 +0200

    add sample notebook for formatting

---
+++ samples/format.py
@@ -0,0 +1,43 @@
+# ♇ version 0.1.0
+# This is a valid Python file, but it's best viewed using Pluto
+# Get Pluto from https://github.com/malyvsen/Pluto.py
+
+
+# ♇ cell 24bc3204-0439-42db-bfe0-26a70b27f687
+# This is a sample Pluto notebook to show off the formatting.
+# Comments can appear in cells only - this way, they are editable inside Pluto.
+# Comments beginning with the symbol "♇" are reserved for Pluto.
+
+# ♇ cell e268147d-9ad5-41b3-b7ca-3cbddd1dd9b0
+b = 123 # code is written inline
+
+# ♇ cell e3d2fe6e-8313-485d-9e42-c3056545767b
+# Cells appear in run order, not notebook order.
+# If you view this file in Pluto, this cell will appear above b = 123
+a = b ** 2
+
+# ♇ cell 71fc7147-7082-4b5f-937e-89ef91e9db1c
+2 ** .5 # free-floating expressions are allowed
+
+# ♇ cell 2cc05e8a-b18b-4060-9e5e-d94e23576bb4
+# As are multiple lines of code in a single cell:
+x = 1 + 1j
+y = x ** 2
+z = y + x
+
+# ♇ cell de098848-5f73-46e7-9180-96b404d6f80a
+# Cells with reactivity errors, ie. undefined order, are saved at the bottom
+cycle1 = cycle2
+
+# ♇ cell 21c40fe4-7847-4514-a8bb-2882cc49bc1c
+cycle2 = cycle1
+
+
+# ♇ cell order:
+# ♇ cell 24bc3204-0439-42db-bfe0-26a70b27f687
+# ♇ cell e3d2fe6e-8313-485d-9e42-c3056545767b
+# ♇ cell e268147d-9ad5-41b3-b7ca-3cbddd1dd9b0
+# ♇ cell de098848-5f73-46e7-9180-96b404d6f80a
+# ♇ cell 21c40fe4-7847-4514-a8bb-2882cc49bc1c
+# ♇ cell 71fc7147-7082-4b5f-937e-89ef91e9db1c
+# ♇ cell 2cc05e8a-b18b-4060-9e5e-d94e23576bb4
